cd6e69ae0bdf7f7d4a0fb48a1accd17ccb40f31a
ahmed-ali-2024 <a3a1981@gmail.com>
2025-12-05 16:10:20 +0200

    refactor: Replace Decap CMS with Keystatic and Markdoc for content management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import keystatic from '@keystatic/astro';
+import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
-  integrations: [react()]
+  integrations: [
+    react(),
+    markdoc(),
+    ...(process.env.NODE_ENV !== 'production' ? [keystatic()] : []),
+  ]
 });
